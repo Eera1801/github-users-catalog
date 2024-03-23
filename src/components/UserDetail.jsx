ff9f8c013bde39b287dfddbde4de624a59dfff14
Eera1801 <esakkimuthurajm12@gmail.com>
2024-03-23 21:35:14 +0530

    Task Completed

--- src/components/UserDetail.jsx
+++ src/components/UserDetail.jsx
@@ -1,7 +1,88 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { useLocation } from "react-router-dom";
+import GetData from "../utils/common";
+import Avatar from "@mui/material/Avatar";
+import invalidImage from "../assets/invalidImage.svg";
 
 function UserDetail() {
-  return <div>UserDetail</div>;
+  const location = useLocation();
+  const [details, setDetails] = useState();
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [isInvalid, setIsInvalid] = useState(false);
+  const [isLoding, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    localStorage.setItem("userName", location.state?.data);
+    const userName = localStorage.getItem("userName");
+    GetData(`https://api.github.com/users/${userName}`, "GET")
+      .then((data) => {
+        setDetails(data);
+        setIsLoading(false);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    if (details?.name) {
+      const com = details?.name.split(" ");
+      setIsInvalid(false);
+      setFirstName(com[0]);
+      setLastName(com[1]);
+    } else {
+      setIsInvalid(true);
+    }
+  }, [details]);
+  return (
+    <div>
+      {!isInvalid && <h1 className="heading">GitHub Users Detail</h1>}
+      {details && !isLoding && !isInvalid && (
+        <>
+          <Avatar
+            alt={"avatar image"}
+            sx={{ width: "150px", height: "150px", margin: "auto" }}
+            src={details.avatar_url}
+          />
+          <div className="detail-body">
+            <div className="d-flex">
+              <div className="content-head">First Name </div>
+              <div className="bw-class">:</div>
+              <div className="content-body">{firstName}</div>
+            </div>
+            <div className="d-flex">
+              <div className="content-head">Last Name </div>
+              <div className="bw-class">:</div>
+              <div className="content-body">{lastName}</div>
+            </div>
+            <div className="d-flex">
+              <div className="content-head"> Company </div>
+              <div className="bw-class">:</div>
+              <div className="content-body">
+                {details.company ? details.company : "NA"}
+              </div>
+            </div>
+            <div className="d-flex">
+              <div className="content-head"> Location </div>
+              <div className="bw-class">:</div>
+              <div className="content-body">
+                {details.location ? details.location : "NA"}
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+      {isInvalid && !isLoding && (
+        <div className="invalid-container">
+          <img
+            alt="Invalid-image"
+            className="invalid-image"
+            src={invalidImage}
+          />
+          <h3 className="invalid-text">Invalid User!!</h3>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default UserDetail;
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -4,147 +4,72 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
-import ImageIcon from "@mui/icons-material/Image";
 import { Link } from "react-router-dom";
+import GetData from "../utils/common";
+import pageNotFound from "../assets/pageNotFound.svg";
 
 function UserList() {
   const [userData, setUserData] = useState(null);
+  const [isLoding, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const responseData = [
-      {
-        login: "octocat",
-        id: 1,
-        node_id: "MDQ6VXNlcjE=",
-        avatar_url: "https://github.com/images/error/octocat_happy.gif",
-        gravatar_id: "",
-        url: "https://api.github.com/users/octocat",
-        html_url: "https://github.com/octocat",
-        followers_url: "https://api.github.com/users/octocat/followers",
-        following_url:
-          "https://api.github.com/users/octocat/following{/other_user}",
-        gists_url: "https://api.github.com/users/octocat/gists{/gist_id}",
-        starred_url:
-          "https://api.github.com/users/octocat/starred{/owner}{/repo}",
-        subscriptions_url: "https://api.github.com/users/octocat/subscriptions",
-        organizations_url: "https://api.github.com/users/octocat/orgs",
-        repos_url: "https://api.github.com/users/octocat/repos",
-        events_url: "https://api.github.com/users/octocat/events{/privacy}",
-        received_events_url:
-          "https://api.github.com/users/octocat/received_events",
-        type: "User",
-        site_admin: false,
-        name: "monalisa octocat",
-        company: "GitHub",
-        blog: "https://github.com/blog",
-        location: "San Francisco",
-        email: "octocat@github.com",
-        hireable: false,
-        bio: "There once was...",
-        twitter_username: "monatheoctocat",
-        public_repos: 2,
-        public_gists: 1,
-        followers: 20,
-        following: 0,
-        created_at: "2008-01-14T04:33:35Z",
-        updated_at: "2008-01-14T04:33:35Z",
-      },
-      {
-        login: "octocat",
-        id: 2,
-        node_id: "MDQ6VXNlcjE=",
-        avatar_url: "",
-        gravatar_id: "",
-        url: "https://api.github.com/users/octocat",
-        html_url: "https://github.com/octocat",
-        followers_url: "https://api.github.com/users/octocat/followers",
-        following_url:
-          "https://api.github.com/users/octocat/following{/other_user}",
-        gists_url: "https://api.github.com/users/octocat/gists{/gist_id}",
-        starred_url:
-          "https://api.github.com/users/octocat/starred{/owner}{/repo}",
-        subscriptions_url: "https://api.github.com/users/octocat/subscriptions",
-        organizations_url: "https://api.github.com/users/octocat/orgs",
-        repos_url: "https://api.github.com/users/octocat/repos",
-        events_url: "https://api.github.com/users/octocat/events{/privacy}",
-        received_events_url:
-          "https://api.github.com/users/octocat/received_events",
-        type: "User",
-        site_admin: false,
-        name: "monalisa octocat",
-        company: "GitHub",
-        blog: "https://github.com/blog",
-        location: "San Francisco",
-        email: "octocat@github.com",
-        hireable: false,
-        bio: "There once was...",
-        twitter_username: "monatheoctocat",
-        public_repos: 2,
-        public_gists: 1,
-        followers: 20,
-        following: 0,
-        created_at: "2008-01-14T04:33:35Z",
-        updated_at: "2008-01-14T04:33:35Z",
-      },
-    ];
-    setUserData(responseData);
+    GetData("https://api.github.com/users", "GET")
+      .then((data) => {
+        setUserData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => console.error(error));
   }, []);
 
-  function stringToColor(string) {
-    let hash = 0;
-    let i;
-
-    /* eslint-disable no-bitwise */
-    for (i = 0; i < string.length; i += 1) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-
-    let color = "#";
-
-    for (i = 0; i < 3; i += 1) {
-      const value = (hash >> (i * 8)) & 0xff;
-      color += `00${value.toString(16)}`.slice(-2);
-    }
-    /* eslint-enable no-bitwise */
-
-    return color;
-  }
-
-  function stringAvatar(name) {
-    return {
-      sx: {
-        bgcolor: stringToColor(name),
-      },
-      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
-    };
-  }
-
   return (
-    <div>
-      {userData?.map((data) => (
-        <List
-          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
-          key={data.id}
-        >
-          <ListItem>
-            <ListItemAvatar>
-              {data.avatar_url ? (
-                <Avatar alt={"avatar image"} src={data.avatar_url} />
-              ) : (
-                <Avatar
-                  sx={{ height: 45, width: 45 }}
-                  {...stringAvatar(data.name)}
+    <>
+      {userData && !isLoding && <h1 className="heading">GitHub Users List</h1>}
+      <div className="row">
+        {userData
+          ? userData?.map((data) => (
+              <List
+                sx={{
+                  width: "100%",
+                  maxWidth: 260,
+                  bgcolor: "background.paper",
+                  margin: "1rem",
+                  borderRadius: "5px",
+                  boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+                }}
+                key={data.id}
+              >
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar alt={"avatar image"} src={data.avatar_url} />
+                  </ListItemAvatar>
+                  <div>
+                    <ListItemText
+                      sx={{ textTransform: "capitalize" }}
+                      primary={data.login}
+                    />
+                    <Link
+                      className="link"
+                      to={"/user-detail"}
+                      state={{ data: data.login }}
+                    >
+                      See Details
+                    </Link>
+                  </div>
+                </ListItem>
+              </List>
+            ))
+          : !isLoding && (
+              <div className="invalid-container">
+                <img
+                  alt="Invalid-image"
+                  className="invalid-image"
+                  src={pageNotFound}
                 />
-              )}
-            </ListItemAvatar>
-            <div>
-              <ListItemText primary={data.name} />
-              <Link to={"/user-detail"}>See Details</Link>
-            </div>
-          </ListItem>
-        </List>
-      ))}
-    </div>
+                <h3 className="invalid-text">API Failure</h3>
+              </div>
+            )}
+      </div>
+    </>
   );
 }
 
